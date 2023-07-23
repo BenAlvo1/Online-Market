abb1014cd298d7f504b8b65e2e50ddd9927ded0c
Ben <Alvo@post.bgu.ac.il>
2023-07-23 11:09:29 +0300

    pushing project

---
+++ Market/ClientMarket/src/Objects/Member.tsx
@@ -0,0 +1,13 @@
+class Member {
+  id: number;
+  userName: string;
+  loggedIn: boolean;
+
+  constructor(id: number, name: string, loggedIn: boolean) {
+    this.id = id;
+    this.userName = name;
+    this.loggedIn = loggedIn;
+  }
+}
+
+export default Member;
---
+++ Market/Market/DataLayer/DTOs/AppointmentDTO.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Market.DomainLayer;
+
+namespace Market.DataLayer.DTOs
+{
+    [Table("Appointments")]
+    public class AppointmentDTO
+    {
+        [Key]
+        [ForeignKey("MemberDTO")]
+        public int MemberId { get; set; }
+        [Key]
+        [ForeignKey("ShopDTO")]
+        public int ShopId { get; set; }
+        public MemberDTO? Appointer { get; set; }
+        public List<MemberDTO> Appointees { get; set; }
+        public string Role {get; set; }
+        public int Permissions { get; set; }
+
+        public AppointmentDTO() { }
+        public AppointmentDTO(Appointment appointment) {
+            lock (MarketContext.MarketContextLock)
+            {
+                MarketContext context = MarketContext.GetInstance();
+                MemberId = appointment.Member.Id;
+                ShopId = appointment.Shop.Id;
+                if (appointment.Appointer != null)
+                    Appointer = context.Members.Find(appointment.Appointer.Id);
+                Appointees = new List<MemberDTO>();
+                foreach (Member member in appointment.Apointees)
+                    Appointees.Add(context.Members.Find(member.Id));
+                Role = appointment.Role.ToString();
+                Permissions = (int)appointment.Permissions;
+            }
+        }
+
+        public AppointmentDTO(int memberId, int shopId, MemberDTO appointer, List<MemberDTO> appointees, string role, int permissions)
+        {
+            MemberId = memberId;
+            ShopId = shopId;
+            Appointer = appointer;
+            Appointees = appointees;
+            Role = role;
+            Permissions = permissions;
+        }
+    }
+}
